803d7caba70913ed4378dda41eb44c24c3b1231b
fdev4305 <fdev4305@gmail.com>
2024-08-02 21:51:36 +0500

    math randon css gacha bo`lgan qismi

---
+++ app.js
@@ -0,0 +1,51 @@
+let button1 = document.querySelector('#myButton1')
+let button2 = document.querySelector('#myButton2')
+let button3 = document.querySelector('#myButton3')
+let button4 = document.querySelector('#myButton4')
+
+
+
+//  birchi son uchunni randam qilish uchun 
+const son1 = document.querySelector('#a');
+const sonA = Math.trunc(Math.random() * 50 + 1);
+son1.textContent = sonA; // sonA ni ekranga chiqarish
+
+//  ikkinchi son uchunni randam qilish uchun 
+const son2 = document.querySelector('#b');
+const sonB = Math.trunc(Math.random() * 40 + 1);
+son2.textContent = sonB; // sonB ni ekranga chiqarish
+
+const ishora = document.querySelector('#amal') // ishora qismi uchu o`zgaruvchi
+
+const sonD = document.querySelector('#d') // d 3-son uchun value
+
+// qo`shish uchun kod
+button1.addEventListener('click', () => {
+    ishora.textContent = `+`
+    sonD.textContent = sonA + sonB
+})
+
+button2.addEventListener('click', () => {
+    ishora.textContent = `-`
+    sonD.textContent = sonA - sonB
+})
+
+button3.addEventListener('click', () => {
+    ishora.textContent = `*`
+    sonD.textContent = sonA * sonB
+})
+
+button4.addEventListener('click', () => {
+    ishora.textContent = `/`
+    sonD.textContent = Math.trunc(sonA / sonB)
+})
+
+
+
+
+
+
+console.log(sonA + sonB);
+console.log(sonA - sonB);
+console.log(sonA * sonB);
+console.log(Math.trunc(sonA / sonB));
